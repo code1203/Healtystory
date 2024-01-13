c9bde1b12c56d0c6c66691559ee0be602e2433dd
code1203 <suradath.ruean@northbkk.ac.th>
2024-01-13 14:23:49 +0700

    healty

---
+++ app/calorie/page.js
@@ -0,0 +1,7 @@
+import Calorie from "@/components/Blogs/Calorie"
+const cal = () => {
+    return (
+        <Calorie />
+    )
+}
+export default cal
---
+++ app/rule/page.js
@@ -0,0 +1,7 @@
+import Rulepage from "@/components/Blogs/Rulepage"
+const Rule = () => {
+    return (
+        <Rulepage />
+    )
+}
+export default Rule
